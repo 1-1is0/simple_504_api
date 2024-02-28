54560f46f58739092d42e6fa7055ec0d679be7dc
1-1is0 <amirftma@gmail.com>
2024-02-28 23:13:40 +0330

    add modles

--- lesson/models.py
+++ lesson/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class LessonModel(models.Model):
+    name = models.CharField(_("lesson name"), max_length=128)
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("LessonModel")
+        verbose_name_plural = _("LessonModels")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.pk} {self.name}"
+
+    def get_absolute_url(self):
+        return reverse("LessonModel_detail", kwargs={"pk": self.pk})
+
+
+class UnitModel(models.Model):
+    name = models.CharField(_("unit name"), max_length=50)
+    lesson = models.ForeignKey(
+        "lesson.LessonModel", verbose_name=_("Lesson"), on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("UnitModel")
+        verbose_name_plural = _("UnitModels")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.pk} {self.name}"
+
+    def get_absolute_url(self):
+        return reverse("UnitModel_detail", kwargs={"pk": self.pk})
+
+
+class WordModel(models.Model):
+    unit = models.ForeignKey(
+        "lesson.UnitModel", verbose_name=_(""), on_delete=models.CASCADE
+    )
+    word = models.CharField(_("word"), max_length=128)
+    part_of_speech = models.CharField(_("part of speech"), max_length=50)
+    definition = models.TextField(_("definition"))
+    examples = models.TextField(_("examples"))
+    image = models.ImageField(_("image"), upload_to="word_images/", blank=True)
+
+    # this fields are not applicable to all words
+    adjective = models.CharField(_("adjective"), max_length=50, blank=True)
+    noun = models.CharField(_("noun"), max_length=50, blank=True)
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("WordModel")
+        verbose_name_plural = _("WordModels")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.pk} {self.word}"
+
+    def get_absolute_url(self):
+        return reverse("WordModel_detail", kwargs={"pk": self.pk})
--- my_user/models.py
+++ my_user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+# class UserStudyWordModels()
+class UserStudyWordModel(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_("user"), on_delete=models.CASCADE
+    )
+    word = models.ForeignKey(
+        "lesson.WordModel", verbose_name=_("word"), on_delete=models.CASCADE
+    )
+    right_count = models.PositiveIntegerField(_("right count"), default=0)
+    wrong_count = models.PositiveIntegerField(_("wrong count"), default=0)
+    note = models.TextField(_("note"), blank=True)
+
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    @property
+    def total_count(self):
+        return self.right_count + self.wrong_count
+
+    class Meta:
+        verbose_name = _("UserStudyWordModel")
+        verbose_name_plural = _("UserStudyWordModels")
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.pk} user: {self.user} word: {self.word}"
+
+    def get_absolute_url(self):
+        return reverse("UserStudyWordModel_detail", kwargs={"pk": self.pk})
