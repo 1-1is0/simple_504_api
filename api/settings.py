db0deb594222a6961ab2bc2102fb22309d07c96a
amir <amirftma@gmail.com>
2024-03-02 20:19:25 +0330

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -96,10 +96,11 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 # django-cors-headers
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:3000",
-#     "http://localhost:3000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "https://simple504api.darkube.app",
+]
 CORS_ORIGIN_ALLOW_ALL = True
 
 CSRF_TRUSTED_ORIGINS = [
