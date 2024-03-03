32765c5176d0c2a1a34f0c235fc8512cdefe7203
1-1is0 <amirftma@gmail.com>
2024-03-03 16:51:18 +0330

    update user state

--- my_user/views.py
+++ my_user/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from my_user.models import UserStudyWordModel
 from my_user.serializers import UserStudyWordSerializer
 
@@ -6,3 +8,19 @@ from my_user.serializers import UserStudyWordSerializer
 class UserStudyWordViewSet(viewsets.ModelViewSet):
     queryset = UserStudyWordModel.objects.all()
     serializer_class = UserStudyWordSerializer
+
+    @action(detail=True, methods=["get"])
+    def correct(self, request, pk=None):
+        user_study_word = self.get_object()  # type: UserStudyWordModel
+        user_study_word.right_count += 1
+        user_study_word.save()
+        serializer = self.get_serializer(user_study_word)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["get"])
+    def wrong(self, request, pk=None):
+        user_study_word = self.get_object()  # type: UserStudyWordModel
+        user_study_word.wrong_count += 1
+        user_study_word.save()
+        serializer = self.get_serializer(user_study_word)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
